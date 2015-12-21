0111d4c2733a513f7f436168c013d34112c7d059
Richard Dallaway <richard@dallaway.com>
2015-12-21 16:51:02 +0000

    Added support for E.Scala Foreword

--- eslick.sh
+++ eslick.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 BOOKDIR=../essential-slick/dist
 STEM=essential-slick-3
-mv -v $BOOKDIR/$STEM-preview.pdf $BOOKDIR/$STEM-preview-raw.pdf
-sbt "run $BOOKDIR/$STEM.pdf $BOOKDIR/$STEM-preview-raw.pdf $BOOKDIR/$STEM-preview.pdf"
-rm $BOOKDIR/$STEM-preview-raw.pdf
-open $BOOKDIR/$STEM-preview.pdf
+sbt "run $BOOKDIR/$STEM.pdf $BOOKDIR/$STEM-preview.pdf $BOOKDIR/$STEM-preview-with-full-toc.pdf"
+open $BOOKDIR/$STEM-preview-with-full-toc.pdf
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,19 +1,22 @@
 import com.itextpdf.text.Document
-import com.itextpdf.text.pdf.{ PdfCopy, PdfReader}
-import com.itextpdf.text.pdf.parser.{ PdfReaderContentParser, SimpleTextExtractionStrategy}
-import java.io.FileOutputStream
+import com.itextpdf.text.pdf.{PdfCopy, PdfReader}
+import com.itextpdf.text.pdf.parser.{PdfReaderContentParser, SimpleTextExtractionStrategy}
+import java.io.{File, FileOutputStream}
+import java.util.regex.Pattern
 
 // The idea is to locate the TOC page numbers
 case class InclusivePageRange(first: Int, last: Int)
 
 // Once we have the page numbers we will splice the TOC from the source (full PDF),
-// replacing the TOC in the destination (preview PDF)
+// replacing the TOC in the destination (preview PDF).
+// That is we select certain page from the source, and
+// in the destination we replace certain pages with the selecte source pages.
 case class Splice(
   source: PdfReader, select:  InclusivePageRange,
   dest:   PdfReader, replace: InclusivePageRange
 )
 
-// Splicing will create a new document, not modify an existing one
+// I say "replace", but Splicing will create a new document, not modify an existing one
 object Editor {
   def apply(command: Splice, outputFilename: String): Unit  = {
     val doc = new Document()
@@ -23,7 +26,7 @@ object Editor {
     import command._
     (1 until replace.first).foreach(from(dest))
     (select.first to select.last).foreach(from(source))
-    (replace.last to dest.getNumberOfPages).foreach(from(dest))
+    (replace.last+1 to dest.getNumberOfPages).foreach(from(dest))
     doc.close
   }
 }
@@ -31,21 +34,25 @@ object Editor {
 // In our PDFs the TOC starts on page 3.
 // The problem, then, is locating the end of the TOC.
 
-// We do that by searching for a page that starts with the word "Preface".
-// The preface is the first page after the TOC, so give us the end of the TOC.
+// We do that by searching for a page that starts with the word "Preface" or "Foreword".
+// The preface (or foreword) is the first page after the TOC, so give us the end of the TOC.
 object TOCFinder {
 
-  // Does the given page in the given document start with the given text?
-  def hasTitle(text: String, doc: PdfReader)(pageIndex: Int): Boolean = {
+  val regex = Pattern.compile("^(Foreword|Preface).*", Pattern.DOTALL)
+
+  // Does the given page in the given document start with the given regex?
+  def hasTitle(pattern: Pattern, doc: PdfReader)(pageIndex: Int): Boolean = {
     val parser = new PdfReaderContentParser(doc)
+    // Null pointer here? We've probably run out pages looking for the pattern
     val content = parser.processContent(pageIndex, new SimpleTextExtractionStrategy()).getResultantText()
-    content.trim.startsWith(text)
+    //println(s"\n\n$pageIndex: ${content.trim}")
+    pattern.matcher(content.trim).matches()
   }
 
   def apply(doc: PdfReader): Option[InclusivePageRange] = {
     val tocStartPage = 3
-    val preface = hasTitle("Preface", doc) _
-    Stream.from(tocStartPage+1).find(preface).map { endPage =>
+    val justAfterToc = hasTitle(regex, doc) _
+    Stream.from(tocStartPage+1).find(justAfterToc).map { endPage =>
       InclusivePageRange(tocStartPage, endPage-1)
     }
   }
@@ -56,6 +63,9 @@ object Toctastic extends App {
   args match {
     case Array(fullFilename, previewFilename, outputFilename) =>
 
+      assert(new File(fullFilename).exists,    s"Cannot find $fullFilename")
+      assert(new File(previewFilename).exists, s"Cannot find $previewFilename")
+
       val full = new PdfReader(fullFilename)
       val preview = new PdfReader(previewFilename)
 
@@ -64,10 +74,10 @@ object Toctastic extends App {
         previewToc <- TOCFinder(preview)
       } yield Splice(full, fullToc, preview, previewToc)
 
-      // println(command)
+      println(command)
 
       command.fold(println("Unable to find TOCs"))(Editor(_, outputFilename))
 
-    case _ => println("Usage: full.psd preview.pdf out.pdf")
+    case _ => println("Usage: full.pdf preview.pdf out.pdf")
   }
 }
